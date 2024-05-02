b110f6e6e5e57a0bca3f4c4eb033380d98a9609f
Cesar-230405 <146404691+Cesar-230405@users.noreply.github.com>
2024-05-01 22:18:23 -0600

    CRUD completo

--- Traductor.cpp
+++ Traductor.cpp
@@ -19,7 +19,7 @@ vector<string> split(const string& s, char delimiter) {
 
 void menu(){
 	
-	cout<<"Elija una opción"<<endl;
+	cout<<"\nElija una opción"<<endl;
 	cout<<"1-Crear"<<endl;
 	cout<<"2-Leer"<<endl;
 	cout<<"3-Editar"<<endl;
@@ -71,6 +71,94 @@ void Leer(){
 		cout<<"Syntaxis Error"<<endl;
 	}
 }
+void Editar(){
+	fstream archivo("translate.txt", ios::in | ios::out);
+	if(archivo.is_open()){
+		string palabra, npalabra, ntraduccion, nfuncion, linea;
+		bool encontro=false;
+		cout<<"Ingrese la palabra a editar: ";
+		cin>>palabra;
+		while(getline(archivo,linea)){
+			vector<string>palabras=split(linea, ',');
+			if(palabras.size() == 3 && palabras[0] == palabra){
+				encontro=true;
+				cout<<"Ingrese la nueva palabra: ";
+				cin>>npalabra;
+				cout<<"Ingrese la nueva traduccion: ";
+				cin>>ntraduccion;
+				cout<<"Ingrese la nueva funcion: ";
+				cin.ignore();
+				getline(cin, nfuncion);
+				linea=npalabra+","+ntraduccion+","+nfuncion;
+				break;
+			}	
+		}
+		if(encontro){
+			ofstream temp("translate_temp.txt");
+			if(temp.is_open()){
+			    archivo.clear();
+                archivo.seekg(0);
+				string linea_original;
+				while(getline(archivo, linea_original)){
+					vector<string>spliteado=split(linea_original, ',');
+					if(spliteado[0]!=palabra){
+						temp<<linea_original<<endl;
+					}
+				}							
+				temp << linea << endl;
+				temp.close();
+				archivo.close();
+				remove("translate.txt");
+				rename("translate_temp.txt","translate.txt");
+				cout<<"\nEditado exitosamente :)";
+			}
+		}else{
+			cout<<"No fue posible aperturar el archivo";
+		}
+	}else{
+		cout<<"No fue posible aperturar el archivo";
+	}
+	
+}
+void Eliminar(){
+	fstream archivo("translate.txt", ios::in | ios::out);
+	if(archivo.is_open()){
+		string palabra, linea;
+		bool encontro=false;
+		cout<<"Ingrese la palabra a eliminar: ";
+		cin>>palabra;
+		while(getline(archivo,linea)){
+			vector<string>palabras=split(linea, ',');
+			if(palabras.size() == 3 && palabras[0] == palabra){
+				encontro=true;
+				break;
+			}	
+		}
+		if(encontro){
+			ofstream temp("translate_temp.txt");
+			if(temp.is_open()){
+			    archivo.clear();
+                archivo.seekg(0);
+				string linea_original;
+				while(getline(archivo, linea_original)){
+					vector<string>spliteado=split(linea_original, ',');
+					if(spliteado[0]!=palabra){
+						temp<<linea_original<<endl;
+					}
+				}							
+				temp.close();
+				archivo.close();
+				remove("translate.txt");
+				rename("translate_temp.txt","translate.txt");
+				cout<<"\nEliminado exitosamente :)";
+			}
+		}else{
+			cout<<"No fue posible aperturar el archivo";
+		}
+	}else{
+		cout<<"No fue posible aperturar el archivo";
+	}
+}
 
 int main(){
 	int opcion=0;
@@ -85,9 +173,11 @@ int main(){
 			case 2: 
 				Leer();
 			break;
-			case 3: 
+			case 3:
+				Editar(); 
 			break;
 			case 4: 
+				Eliminar();
 			break;
 			case 5: 
 				
